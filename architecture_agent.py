49a1fea4b6242b132ee3f40a1f48159477616105
Matthieu <matt1607@hotmail.fr>
2025-01-31 13:24:46 +0100

    refactor: Improve documentation and JSON handling in architecture_agent.py

--- architecture_agent.py
+++ architecture_agent.py
@@ -4,6 +4,7 @@ from smolagents.agents import ToolCallingAgent
 from smolagents import tool, ManagedAgent
 from typing import Optional
 from ai_agent import model
+import json
 
 load_dotenv()
 
@@ -12,7 +13,7 @@ def get_datasheet_content() -> str:
     """
     Returns:
 
-        str: the content of all documents, which will be used to define architecture
+        str: the content of all documents, it is raw data which will be used to define architecture
     """
     content = read_datasheet_contents(folder_path='./Data/Datasheet')
     output = f"""## Documentation is the following {content} \n"""
@@ -63,17 +64,20 @@ def check_architecture_requirements(Architectures: str) -> str:
 
     Args:
         Architectures: a list of all architectures found, shall follow the following structure
-            {[{Name: a name or title for this architecture, like "1x TTC 32"},
-            {Reference: the ECU reference, as an example TTC 580},
-            {Number: number of this reference needed to cover the need, shall be a number},
-            {Software: the platform, can only be C, MATCH, Codesys, Qt}]}
+            {[
+            {Name: a name or title for this architecture, like "1x TTC 32",
+            Reference: the ECU reference, as an example TTC 580,
+            Number: number of this reference needed to cover the need, shall be a number,
+            Software: the platform, can only be C, MATCH, Codesys, Qt}
+            ]}
 
     Returns:
         str: the missing information it there is one, else success, mandatory data has been provided
     """
     # Temporary implementation to avoid syntax errors
     missing_info = []
-    for architecture in Architectures:
+    data = json.loads(Architectures)
+    for architecture in data:
         missing = []
         if not architecture.get("Name"):
             missing.append("Name")
@@ -95,44 +99,21 @@ managed_architecture_define_agent = ManagedAgent(
     agent=agent,
     name="architecture_designer",
     description="""You will be tasked to help design and promote the best TTControl ECU architecture""",
-    additional_prompting ="""You can use following documents to better know the product portfolio and based your answer.
-    To work you need to know at least the number of outputs, CAN as well as the software used to develop.
-    First you need to check you have enough information to answer, if no, ask the user to provide them
-    Next define the best TTControl ECU architecture
-    If there is more than one possibility, limit your answer to 3 solutions which shall be different.
-    Few examples below, answer using JSON format
-    Example 1
-    {
-        "Input": 11 AI + 7 DO + 1 PWM, 3 CAN, Codesys,
-            "Output": [
-            {
-              "Reference": "HY-TTC 32",
-              "Number": "1",
-              "Programming": "Codesys"
-            }
-        ]
-    }
-    Example 2
-    {
-    "Input": 21 AI + 7 DO + 16 PWM, C or Codesys
-        "Output": [
-        {
-          "Reference": "1x HY-TTC 510",
-          "Number": "1",
-          "Software": "Codesys"
-        },
-        {
-          "Reference": "1x TTC 2310 (TTC 2785 variant)",
-          "Number": "1",
-          "Software": "C"
-        },
-        {
-          "Reference": "2x TTC 32",
-          "Number": "2",
-          "Software": "Codesys"
-        }
-        ]
-    }
+    additional_prompting ="""You have to follow this steps to define the better solution:
+        1: check if there is no missing arguments, you have to have enough information
+        2: get all technical information like datasheet and all documentation to better know the ECU product portfolio
+        3: define best solutions, provide 3 solutions in JSON format and check that all arguments have been provided
+        
+        As soon as you have a JSON list with 3 solutions, you can return this list in JSON format
+        
+        below is an example of expected output
+        
+        {[
+            {Name: "1x TTC 32",
+            Reference: "TTC 32",
+            Number: "1",
+            Software: "Codesys"}
+        ]}
     """
 )
 
